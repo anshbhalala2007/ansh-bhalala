a96d062c8f0928a22b406bee33e2a520645aa247
anshbhalala2007 <bhalalaansh016@gmail.com>
2024-12-02 02:25:28 -0800

    Add files via upload

---
+++ PR_B_FILE.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    FILE *p;
+    int marks,i,n;
+    p=fopen("Marks.txt","w");
+    printf("Enter the number of student :");
+    scanf("%d",&n);
+    printf("Enter the marks of %d students :\n",n);
+    for(i=0;i<n;i++)
+    {
+        printf("Student %d :",i+1);
+        scanf("%d",&marks);
+        putw(marks,p);
+    }
+    fclose(p);
+    printf("Marks have been written to the 'marks.txt'.\n");
+    p=fopen("Marks.txt","r");
+    if(p==NULL);
+    {
+        printf("Marks.txt file does not exists.");
+        return 1;
+    }
+    while((marks = getw(p)) !=EOF)
+    {
+        printf("Student %d : %d\n",i,marks);
+        i++;
+    }
+    fclose(p);
+    return 0;
+}
